ccb3754492837c7b93fc0e13819d0ae6b9aebb38
Allan Amaral <allanmaral@hotmail.com>
2019-04-28 01:34:32 -0300

    Added log system

---
+++ Spixii/src/Spixii/Log.cpp
@@ -0,0 +1,21 @@
+#include "Log.h"
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+namespace Spixii
+{
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ApplicationLogger;
+
+	void Log::Init()
+	{
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+
+		s_CoreLogger   = spdlog::stdout_color_mt("Spixii");
+		s_CoreLogger->set_level(spdlog::level::trace);
+
+		s_ApplicationLogger = spdlog::stdout_color_mt("Application");
+		s_ApplicationLogger->set_level(spdlog::level::trace);
+	}
+}
---
+++ Spixii/src/Spixii/Log.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Core.h"
+
+#include <memory>
+#include <spdlog/spdlog.h>
+
+namespace Spixii
+{
+
+	class SPIXII_API Log
+	{
+	public:
+		static void Init();
+
+		static inline std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		static inline std::shared_ptr<spdlog::logger>& GetApplicationLogger() { return s_ApplicationLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ApplicationLogger;
+	};
+}
+
+#define SPX_CORE_TRACE(...)    ::Spixii::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define SPX_CORE_INFO(...)     ::Spixii::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define SPX_CORE_WARN(...)     ::Spixii::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define SPX_CORE_ERROR(...)    ::Spixii::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define SPX_CORE_CRITICAL(...) ::Spixii::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+#define SPX_TRACE(...)    ::Spixii::Log::GetApplicationLogger()->trace(__VA_ARGS__)
+#define SPX_INFO(...)     ::Spixii::Log::GetApplicationLogger()->info(__VA_ARGS__)
+#define SPX_WARN(...)     ::Spixii::Log::GetApplicationLogger()->warn(__VA_ARGS__)
+#define SPX_ERROR(...)    ::Spixii::Log::GetApplicationLogger()->error(__VA_ARGS__)
+#define SPX_CRITICAL(...) ::Spixii::Log::GetApplicationLogger()->critical(__VA_ARGS__)
